7704089352801fb1bfdd90e7419ba201f3118213
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-04-30 09:00:03 +0530

    Add files via upload

---
+++ Week2b.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.String;
+  class Week2b
+{
+   static int add(int a,int b,int c)
+  {
+   return a+b+c;
+  }
+   static double add(double a,double b,double c)
+ {
+   return a+b+c;
+  }
+
+
+
+
+public static void main(String args[])
+ {
+   //Week2b b=new Week2b();
+
+   System.out.println(add(2,3,6));
+   System.out.println(add(5.2,2.5,7.3));
+ }
+}
